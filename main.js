a7f1d78fd79e8baa56634cae8a06f9f44fd97d35
EMMANUEL AKPAN <heisemmaco@gmail.com>
2023-09-12 11:38:27 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 let theHoverButton = document.querySelector('.theHoverButton')
 let theItems = document.getElementById('onClick')
-
+let tg = document.querySelectorAll(
+    "body, main div:not(#onClick) ~ *, main div:not(#onClick) > * "
+  );
+console.log(tg);
 handleClick = () => {
-    if (theItems.classList.contains('whenClick') && theHoverButton.classList.contains('secondhover')) {
-        theItems.classList.replace('whenClick', 'disappear')
-        theHoverButton.classList.replace('secondhover', 'theHoverButton')
-    } else if (theItems.classList.contains('disappear') && theHoverButton.classList.contains('theHoverButton')) {
-        theItems.classList.replace('disappear', 'whenClick') 
-        theHoverButton.classList.replace('theHoverButton', 'secondhover')
-    }
+    theItems.classList.replace('disappear', 'whenClick') 
+    theHoverButton.classList.replace('theHoverButton', 'secondhover')
 }
 
 theHoverButton.addEventListener('click', handleClick)
 
-
+window.onclick = (e) => {
+    if (theItems.classList.contains("whenClick")) {
+      for (i = 0; i < tg.length; i++) {
+        if (e.target === tg[i]) {
+          //console.log("something happened?");
+          theItems.classList.replace('whenClick', 'disappear')
+          theHoverButton.classList.replace('secondhover', 'theHoverButton')
+        }
+      }
+    }
+  };
 
 
